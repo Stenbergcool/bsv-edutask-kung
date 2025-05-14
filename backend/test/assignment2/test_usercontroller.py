adff75746122de069891ec06f41fdd057e47f411
Björn Horndahl <bjorn9111@gmail.com>
2025-05-15 00:49:29 +0200

    Add unit mark to all unit tests

--- backend/test/assignment2/test_usercontroller.py
+++ backend/test/assignment2/test_usercontroller.py
@@ -22,22 +22,27 @@ def user_controller(mock_dao):
     {'email': 'test@test.com'},
     [1, 2, 3, "4"]
 ])
+
+@pytest.mark.unit
 def test_get_user_by_email_invalid_email(user_controller, email):
     with pytest.raises(ValueError, match='Error: invalid email address'):
         user_controller.get_user_by_email(email)
 
+@pytest.mark.unit
 # Test case for when theres no used returned from database method call - should return None
 def test_get_user_by_email_no_user(user_controller):
     user_controller.dao.find.return_value = []
     result = user_controller.get_user_by_email('test@test.com')
     assert result is None
 
+@pytest.mark.unit
 # Test case for when the database method throws an exception - should raise the same exception
 def test_get_user_by_email_database_throw(user_controller):
     user_controller.dao.find.side_effect = Exception("Database error")
     with pytest.raises(Exception, match='Database error'):
         user_controller.get_user_by_email('test@test.com')
 
+@pytest.mark.unit
 # Test case for when there are multiple users with the same email - should print an error message and return the email
 def test_get_user_by_email_two_user_same_email_prints_error(user_controller, capfd):
     user_controller.dao.find.return_value = [ {"email": 'test@test.com' }, {"email": 'test@test.com' } ]
@@ -45,11 +50,13 @@ def test_get_user_by_email_two_user_same_email_prints_error(user_controller, cap
     out, err = capfd.readouterr()
     assert "test@test.com" in out
 
+@pytest.mark.unit
 def test_get_user_by_email_two_user_same_email_returns_user(user_controller, capfd):
     user_controller.dao.find.return_value = [ {"email": 'test@test.com' }, {"email": 'test@test.com' } ]
     result = user_controller.get_user_by_email('test@test.com')
     assert result == {"email": 'test@test.com' }
 
+@pytest.mark.unit
 # Test case for one email that is included in database
 def test_get_user_by_email_valid_user_email(user_controller):
     user_controller.dao.find.return_value = [ {"email": 'test@test.com' } ]
